b2e068f993be9b5fb3bf8e22349ae77ddf16e740
Enzo <misticrdz@gmail.com>
2024-03-18 22:09:07 +0800

    first commit

---
+++ resources/js/Components/ApplicationLogo.jsx
@@ -0,0 +1,7 @@
+import Logo from './STI-Logo.png';
+export default function ApplicationLogo(props) {
+    return (
+       <img src={Logo} className='w-30' alt="" />
+    );
+}
+
---
+++ resources/js/Components/STILogo.jsx
@@ -0,0 +1,6 @@
+import Logo from './STI-Logo.png';
+export default function STILogo(props) {
+    return (
+       <img src={Logo} className='w-20' alt="" />
+    );
+}

bffb9cef797056fe3de5dfbbeb8fc1a4eeef471a
codemedavid <johnangelo.programming@gmail.com>
2024-05-10 12:23:29 +0800

    Added Subtext to migration

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -31,11 +31,13 @@ class ArticleController extends Controller
     {
         $request->validate([
             'title' => 'required|string|max:255',
+            'subtext' => 'required|string',
             'content' => 'required',
         ]);
 
         Article::create([
             'title' => $request->title,
+            'subtext' => $request->subtext,
             'content' => $request->content,
         ]);
 
